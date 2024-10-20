09032d0de5211820b7ff3febc380fe11035ed202
goyalshivansh2805 <144095961+goyalshivansh2805@users.noreply.github.com>
2024-10-20 13:20:05 +0530

    Added checks for logs directory to exits

--- middlewares/logRequests.js
+++ middlewares/logRequests.js
@@ -2,11 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 const logRequests = (req, res, next) => {
-    const { method, url,ip } = req;
+    const { method, url, ip } = req;
     const origin = req.headers.origin || 'unknown';
-    const log = `${method} ${url} ${origin} ${new Date} ${ip}\n`;
-    const logFilePath = path.join(__dirname, '../logs/requests.txt');
-    
+    const log = `${method} ${url} ${origin} ${new Date().toISOString()} ${ip}\n`;
+    const logsDir = path.join(__dirname, '../logs');
+    const logFilePath = path.join(logsDir, 'requests.txt');
+    if (!fs.existsSync(logsDir)) {
+        fs.mkdirSync(logsDir, { recursive: true }); 
+    }
+    if (!fs.existsSync(logFilePath)) {
+        fs.writeFileSync(logFilePath, ''); 
+    }
     fs.appendFile(logFilePath, log, (err) => {
         if (err) {
             console.log(err);
